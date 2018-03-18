2f56d9445c0dbfa46d0422a0895785e8a3fe6baa
timmi <timmi@localhost.localdomain>
2018-03-18 23:48:27 +0100

    0

--- 2.pl
+++ 2.pl
@@ -3,12 +3,12 @@ max(X,Y,Z):-
   X>=Y,Z is X;
   Y>X, Z is Y.
 
-max_sum(Ls,S):-
-max_sum(Ls,_,S).
+max_sum([L|Ls],S):-
+max_sum(Ls,L,L,S).
 
-max_sum([L], L, L).
+max_sum([],_,X,X).
 
-max_sum([L|Ls],Here, Before):-
-  max_sum(Ls, New_Here, New_Before),
-  T1 is New_Here+L,max(L,T1,T2),Here is T2,
-  Before is max(New_Before,Here).
+max_sum([L|Ls],Here, Before,O):-
+  T1 is Here+L,max(L,T1,T2),New_Here is T2,
+  New_Before is max(Before,New_Here),
+  max_sum(Ls, New_Here, New_Before,O).
